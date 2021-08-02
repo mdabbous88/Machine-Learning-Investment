5940a3f2ee253510af8fe82e1ce780cfb0226e38
sed-jackob <70657965+sed-jackob@users.noreply.github.com>
2021-08-02 16:22:48 -0600

    Updated app.py

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-# API_key = "6358542c8dmsh4b07038f2b6d052p1898fajsne9fbc58d30f6" #Mohammad API
 API_key = "0bc6d85258msh0169da777118572p18eaf1jsnfdb23e2857d9" #Saeed API
-# db_pass = "Odiido76"
 db_pass = "0cee5583cca719f73f55ea5ea820f555b25de9d286a0e7c7d4a5c1f78c05c79e"
